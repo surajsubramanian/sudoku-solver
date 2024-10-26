a9f84de117b3bba81c3a7ba763319117d06effc1
surajsubramanian <30796920+surajsubramanian@users.noreply.github.com>
2024-10-27 02:20:16 +0300

    Upgrade to python 3.12 and use a proper package manager

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,28 +1,21 @@
 import os
 import numpy as np
 import cv2
-import torch
-import pytesseract
+from pytesseract import image_to_string
 root = os.getcwd()
-sudoku_board = []
+
 
 def getNumber(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Otsu Tresholding automatically find best threshold value
-    _, binary_image = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-    # invert the image if the text is white and background is black
-    count_white = np.sum(binary_image > 0)
-    count_black = np.sum(binary_image == 0)
-    if count_black > count_white:
-        binary_image = 255 - binary_image
-    # padding
-    final_image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(255, 255, 255))
-    txt = pytesseract.image_to_string(
-        final_image, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
-    return txt
+    image = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    gry = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thr = cv2.threshold(gry, 127, 255, cv2.THRESH_BINARY_INV)[1]
+    return image_to_string(
+        thr, config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
+
 
 print("-- Check the predicted numbers with the numbers in the sudoku grid \n Press Y or y if you want to change the number of just press Enter\n")
-def digit_recognizer():
+def digit_recognizer(interactive_mode=False):
+    sudoku_board = []
     for i,img_name in enumerate(sorted(os.listdir(os.path.join(root, 'temp')))):
         if '.png' not in img_name:
             continue
@@ -30,7 +23,7 @@ def digit_recognizer():
 #        print(img_path.split('/')[-1])
         row,col = i//9 + 1, i%9 + 1
         print(str(row) + ' x ' + str(col), sep = ' : ')
-        
+
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         number = getNumber(cv2.imread(img_path)).replace('\x0c', '').strip()
         if np.all(img==0) or number not in list(map(str, range(0,10))):
@@ -38,10 +31,12 @@ def digit_recognizer():
         else:
             result = int(number)
         print(result)
-        flag = input('Do you want to change the number ? (Y/N) :')
-        if flag == 'y' or flag == 'Y':
-            result = int(input("Enter the number :"))
-            
+
+        if interactive_mode:
+            flag = input('Do you want to change the number ? (Y/N) :')
+            if flag == 'y' or flag == 'Y':
+                result = int(input("Enter the number :"))
+
         sudoku_board.append(result)
-        
+
     return sudoku_board
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import sys
+import argparse
 from sudoku_solver import sudoku_solver
 from digit_recognizer import digit_recognizer
 from backtracking import backtracking
 
-def main(img_path):
+def main(img_path, interactive_mode=False):
     sudoku_solver(img_path)
-    board = digit_recognizer()
+    board = digit_recognizer(interactive_mode)
     backtracking(board)
 
 if __name__ == '__main__':
-    img_path = sys.argv[1]
-    main(img_path)
+    parser = argparse.ArgumentParser(description='Sudoku Solver')
+    parser.add_argument('-i', '--input', type=str, help='Input image path')
+    parser.add_argument('-m', '--interactive-mode', action='store_true', help='Interactive mode for digit recognition')
+
+    args = parser.parse_args()
+    main(args.input, args.interactive_mode)
